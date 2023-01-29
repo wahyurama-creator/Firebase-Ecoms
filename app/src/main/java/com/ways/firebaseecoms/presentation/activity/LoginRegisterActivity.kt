767e80f01ea6a3c6fd949e33f537c674fadac494
wahyu.rama <wahyu.rama@gramedia.id>
2023-01-29 18:58:34 +0700

    Setup project

---
+++ app/src/main/java/com/ways/firebaseecoms/presentation/activity/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.ways.firebaseecoms.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ways.firebaseecoms.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
